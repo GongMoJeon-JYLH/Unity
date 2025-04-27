7d534d0e1f082eddc0f0f25ffb8e39b0e59ea41f
anditsoon <83330939+anditsoon@users.noreply.github.com>
2025-04-27 16:20:44 +0900

    독후감 창 만듦

---
+++ Assets/Scripts/Rec2ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Rec2ButtonManager : MonoBehaviour
+{
+    public GameObject window_writeBookReport;
+    public GameObject window_generateBookReport;
+
+    public void OnClickStartBookReport()
+    {
+        window_writeBookReport.SetActive(true);
+    }
+
+    public void OnClickSubmitBookReport()
+    {
+        window_generateBookReport.SetActive(true);
+        /// 통신 관련 함수
+    }
+}
