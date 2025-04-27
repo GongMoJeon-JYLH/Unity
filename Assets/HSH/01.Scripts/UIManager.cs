196bba615e5bd2614de5249f0f049557e2551f9a
githeed <135950902+githeed@users.noreply.github.com>
2025-04-27 13:40:41 +0900

    UIFix13

--- Assets/HSH/01.Scripts/UIManager.cs
+++ Assets/HSH/01.Scripts/UIManager.cs
@@ -2,29 +2,39 @@
 using UnityEngine.UI;
 using TMPro;
 using TMPro.Examples;
+using Michsky.MUIP;
 
 
 public class UIManager : MonoBehaviour
 {
     private static UIManager instance = null;
 
+    //public TextMeshProUGUI tmp_Chat;
 
+    // 채팅 을 위한 변수
     public ScrollRect srollChatWindow;
     public GameObject aiChat;
     public GameObject userChat;
-
-    public TextMeshProUGUI tmp_Chat;
     public TMP_InputField inputChat;
     public GameObject content;
+    // 채팅 로딩
+    public GameObject loadingBar;
+    // 채팅 입력
+    public GameObject btn_clickChat;
+
+    // 이름 패널, 채팅 패널
     public GameObject panel_chat;
     public GameObject panel_name;
 
+
+    // 키워드 및 유저타입 
     public GameObject keywordPanel;
     public GameObject text_keyword;
 
-    public GameObject loadingBar;
+    public GameObject userTypePanel;
+    public GameObject btn_switchPanel;
+    bool isPanelActive = true;
 
-    public GameObject btn_clickChat;
 
     void Awake()
     {
@@ -55,7 +65,7 @@ public class UIManager : MonoBehaviour
 
         loadingBar.SetActive(false);
         //tmp_Chat.text = "<color=#00FFFF>추천봇</color>: 혹시 최근에 인상 깊게 읽었던 책이나 관심 있는 주제가 있으신가요?";
-
+        isPanelActive = keywordPanel.activeSelf;
         PanelChange(true);
         //btn_sendChat.onClick.AddListener(HttpManager.Instance.OnClickSendChat);
         //btn_Recommand.onClick.AddListener(HttpManager.Instance.OnClickGetBookRecommendation);
@@ -72,5 +82,22 @@ public class UIManager : MonoBehaviour
         panel_chat.SetActive(!isOn);
     }
 
-   
+    public void OnClickSwitchPanel()
+    {
+        isPanelActive = !isPanelActive;
+        keywordPanel.SetActive(isPanelActive);
+        userTypePanel.SetActive(!isPanelActive);
+
+        if(isPanelActive)
+        {
+            btn_switchPanel.GetComponent<ButtonManager>().buttonText = "키워드!";
+        }
+        else
+        {
+            btn_switchPanel.GetComponent<ButtonManager>().buttonText = "유저타입!";
+        }
+        print(isPanelActive); 
+
+    }
+
 }
